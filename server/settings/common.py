2f3ecbc7e2b082761222a67750dd7e01267658ea
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2018-07-19 20:43:52 -0400

    Settings.

--- server/settings/common.py
+++ server/settings/common.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '..'
+)
 
 
 # Quick-start development settings - unsuitable for production

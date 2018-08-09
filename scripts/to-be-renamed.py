bde2bf8682b3d52290bbbe082f9d98f1f141742f
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2018-08-08 23:10:38 -0400

    Script.

--- scripts/to-be-renamed.py
+++ scripts/to-be-renamed.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
-print('ok')
+import sys
+import os
+
+import django_react
+
+module_directory = os.path.dirname(django_react.__file__)
+conf_directory = os.path.join(module_directory, 'conf')
+mypy = os.path.join(conf_directory, 'mypy.ini')
+
+os.symlink(mypy, 'mypy.ini')

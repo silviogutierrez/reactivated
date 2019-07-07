857f6216334c00641c061df1e33ec73d8d623cb8
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-07-07 14:23:10 -0400

    Bar rewrite is working. Now on to SSR.

--- experiments/plugin.py
+++ experiments/plugin.py
@@ -7,19 +7,18 @@ class CustomPlugin(Plugin):
     def get_type_analyze_hook(self, fullname: str):
         # print(fullname)
         if fullname == 'types.Bar':
-            return analyze_bar
+            return analyze_some_name
 
         return None
 
     def get_function_hook(self, fullname: str):
-        if fullname == 'types.ssr':
+        if False and fullname == 'types.ssr':
             return analyze_ssr
 
         return None
 
 
-# This example shows us rewriting Bar as a type to actually mean Union of [itself, int and str]
-def analyze_bar(ctx):
+def analyze_some_name(ctx):
     return UnionType.make_simplified_union([ctx.api.named_type('types.Bar'), ctx.api.named_type('builtins.int'), ctx.api.named_type('builtins.str')])
     # foo = ctx.api.named_type('builtins.int')
     # foo = ctx.api.named_type('builtins.int')
--- experiments/types.py
+++ experiments/types.py
@@ -1,4 +1,4 @@
-from typing import Mapping, TypeVar, Dict, Any, Callable
+from typing import Mapping, TypeVar, Dict
 
 from mypy_extensions import TypedDict
 
@@ -20,22 +20,15 @@ class Bar:
     pass
 
 
-X = TypeVar("X")
-
-
-def ssr(foo: X) -> X:
-    return foo
-
-
-@ssr
+# @ssr
 def testing_stuff(b: Bar) -> bool:
     return True
 
 
-testing_stuff(Bar())
+testing_stuff(Bar())  # All of these pass because Bar is "re-typed"
 testing_stuff(1)
 testing_stuff("atr")
-testing_stuff(Test)
+testing_stuff(Test)  # Fails of course
 
 
 a = Foo(foo="a")
@@ -48,3 +41,18 @@ a == b == c  # Works
 
 # mapping_test(c)  # Works, of course
 # mapping_test(a)  # Does not work
+
+
+X = TypeVar("X")
+
+
+def ssr(foo: X) -> X:
+    return foo
+
+
+@ssr
+def to_be_ssred() -> bool:
+    return True
+
+
+reveal_type(to_be_ssred)

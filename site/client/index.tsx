415e0f00da6a9b7dd430636fadb8a0006f4cb24b
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-08-30 22:09:25 -0400

    Our first component.

---
+++ site/client/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {hydrate, render} from "react-dom";
+import {setStylesTarget} from "typestyle";
+
+import {Provider} from "reactivated/context";
+
+const props = (window as any).__PRELOADED_PROPS__;
+const context = (window as any).__PRELOADED_CONTEXT__;
+
+if ((module as any).hot) {
+    (module as any).hot.accept();
+}
+
+// tslint:disable-next-line
+const Template = require("client/templates/" + context.template_name).default;
+
+setStylesTarget(document.getElementById("styles-target")!);
+
+hydrate(
+    <Provider value={context}>
+        <Template {...props} />
+    </Provider>,
+    document.getElementById("root"),
+)

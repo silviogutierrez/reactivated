3efbb9a6bf04c8ec083ba7a29079d09943d24191
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-08-31 13:08:35 -0400

    Sample form

--- site/server/urls.py
+++ site/server/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("hello-world/", views.hello_world),
+    path("sample-form/", views.sample_form),
 ]
--- site/server/views.py
+++ site/server/views.py
@@ -2,6 +2,14 @@ from django.template.response import TemplateResponse
 
 from django.http import HttpRequest
 
+from . import forms
+
 
 def hello_world(request: HttpRequest) -> TemplateResponse:
     return TemplateResponse(request, "hello_world.tsx", {})
+
+
+
+def sample_form(request: HttpRequest) -> TemplateResponse:
+    form = forms.SampleForm()
+    return TemplateResponse(request, "sample_form.tsx", {"form": form})

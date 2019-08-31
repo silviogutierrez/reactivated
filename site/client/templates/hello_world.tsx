cad2ea657f2629562dbf19647d5c58cb014c49e3
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-08-31 13:00:13 -0400

    Formatting and more.

--- site/client/templates/hello_world.tsx
+++ site/client/templates/hello_world.tsx
@@ -1,3 +1,16 @@
 import React from "react";
+import {style} from "typestyle";
 
-export default () => <div>Hello world!</div>;
+const styles = {
+    layout: style({maxWidth: 600, margin: "0 auto"}),
+
+    header: style({color: "blue"}),
+} as const;
+
+export default () => (
+    <div className={styles.layout}>
+        <h1 className={styles.header}>Hello world!</h1>
+        <p>I am a paragraph</p>
+        <p>I am another paragraph</p>
+    </div>
+);

e44912def35bba7c17d419bbd16ee13addd71bf0
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-09-05 16:06:54 -0400

    Autocomplete working for forms and formsets.

--- reactivated/forms.py
+++ reactivated/forms.py
@@ -1,12 +1,56 @@
 from .widgets import Autocomplete as Autocomplete
 
+import re
 from django import forms as django_forms
 from django.http import JsonResponse, HttpResponse, HttpRequest
 from django.template.response import TemplateResponse
 
-from typing import TypeVar, cast, Any, Union
+from typing import TypeVar, cast, Any, Union, Dict, Optional, NamedTuple
 
-T = TypeVar('T')
+T = TypeVar("T")
+
+
+class FormOrFormSetDescriptor(NamedTuple):
+    prefix: Optional[str]
+    field_name: str
+
+
+def get_form_or_form_set_descriptor(prefixed_name: str) -> FormOrFormSetDescriptor:
+    FORM_SET_REGEX = "(.*)?(-[0-9]-)(.*)"
+    FORM_REGEX = "(.*)?(-)(.*)"
+
+    form_set_match = re.match(FORM_SET_REGEX, prefixed_name)
+    form_match = re.match(FORM_REGEX, prefixed_name)
+
+    if form_set_match is not None:
+        return FormOrFormSetDescriptor(
+            field_name=form_set_match.groups()[2], prefix=form_set_match.groups()[0]
+        )
+    elif form_match is not None:
+        return FormOrFormSetDescriptor(
+            field_name=form_match.groups()[2], prefix=form_match.groups()[0]
+        )
+
+    return FormOrFormSetDescriptor(field_name=prefixed_name, prefix=None)
+
+
+def get_form_from_form_set_or_form(
+    context_data: Dict[
+        str, Union[django_forms.BaseForm, django_forms.formsets.BaseFormSet, object]
+    ],
+    descriptor: FormOrFormSetDescriptor,
+) -> Optional[django_forms.BaseForm]:
+
+    for key, item in context_data.items():
+        if isinstance(item, django_forms.BaseForm) and item.prefix == descriptor.prefix:
+            return item
+        elif (
+            isinstance(item, django_forms.formsets.BaseFormSet)
+            and item.prefix == descriptor.prefix
+        ):
+            return cast(django_forms.BaseForm, item.empty_form)
+
+    return None
 
 
 def autocomplete(view_func: T) -> T:
@@ -21,23 +65,20 @@ def autocomplete(view_func: T) -> T:
             return response
 
         context_data = response.context_data or {}
-
-        for key, item in context_data.items():
-            if isinstance(item, django_forms.BaseForm):
-                form: Union[django_forms.BaseForm, None] = item
-                break
-        else:
-            form = None
+        descriptor = get_form_or_form_set_descriptor(autocomplete)
+        form = get_form_from_form_set_or_form(context_data, descriptor)
 
         if (
             form is not None
-            and autocomplete in form.fields
-            and isinstance(form.fields[autocomplete], django_forms.ModelChoiceField)
-            and isinstance(form.fields[autocomplete].widget, Autocomplete)
+            and descriptor.field_name in form.fields
+            and isinstance(
+                form.fields[descriptor.field_name], django_forms.ModelChoiceField
+            )
+            and isinstance(form.fields[descriptor.field_name].widget, Autocomplete)
         ):
             results = [
                 {"value": result.pk, "label": str(result)}
-                for result in form.fields[autocomplete].queryset.filter(
+                for result in form.fields[descriptor.field_name].queryset.filter(
                     name__icontains=query
                 )[:50]
             ]
--- tests/autocomplete.py
+++ tests/autocomplete.py
@@ -1,6 +1,7 @@
 import pytest
 
 from server.apps.samples import models
+from reactivated import forms
 
 
 @pytest.mark.django_db
@@ -8,36 +9,24 @@ from server.apps.samples import models
 def test_autocomplete(client):
     models.Composer.objects.create(name="Richard Wagner")
     models.Composer.objects.create(name="Wolfgang Amadeus Mozart")
-    response = client.get("/autocomplete-view/", {"autocomplete": "composer", "query": "Wagner"})
-    assert response.json()['results'][0]['label'] == "Richard Wagner"
-
-
-
-from typing import NamedTuple
-
-class FormOrFormSet(NamedTuple):
-    prefix: str
-    name: str
-
-def get_form_or_form_set_descriptor(field_name: str) -> FormOrFormSet:
-    FORM_SET_REGEX = "(.*)?(-[0-9]-)(.*)"
-    FORM_REGEX = "(.*)?(-[0-9]-)(.*)"
+    response = client.get(
+        "/autocomplete-view/", {"autocomplete": "composer", "query": "Wagner"}
+    )
+    assert response.json()["results"][0]["label"] == "Richard Wagner"
 
 
 def test_prefix_calculation(client):
-    import re
-    FORM_SET_REGEX = "(.*)?(-[0-9]-)(.*)"
+    assert forms.get_form_or_form_set_descriptor("opera_form_set-0-composer_field") == (
+        "opera_form_set",
+        "composer_field",
+    )
 
-    assert re.match(FORM_SET_REGEX, "opera_form_set-0-composer_field").groups() == ('opera_form_set', '-0-', 'composer_field')
-    assert re.match(FORM_SET_REGEX, "opera_form_set-0-0-composer_field").groups() == ('opera_form_set-0', '-0-', 'composer_field')
-    assert re.match(FORM_SET_REGEX, "opera_form_set-name") is None
-    assert re.match(FORM_SET_REGEX, "name") is None
-    return 
-    # assert re.match(FORM_SET_REGEX, "opera_form_set-0-0-composer-field") is not None
+    assert forms.get_form_or_form_set_descriptor("opera_form-composer_field") == (
+        "opera_form",
+        "composer_field",
+    )
 
-    # assert re.split(REGEX, "opera_form_set-0-composer-field") == ["opera_form_set", "composer-field"]
-    foo = re.match(REGEX, "opera_form_set-0-composer-field")
-    foo = re.match(REGEX, "opera_form-name")
-    # foo = re.match(REGEX, "name")
-    # foo = re.match(REGEX, "opera-form-set-0-0-composer-field") # == ["opera_form_set", "composer-field"]
-    breakpoint()
+    assert forms.get_form_or_form_set_descriptor("composer_field") == (
+        None,
+        "composer_field",
+    )

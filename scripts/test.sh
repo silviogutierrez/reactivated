c5787cbf1a6ba67aac1ad8f36d6001beb0e4c3f1
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-09-25 22:55:27 -0400

    Testing scripts.

---
+++ scripts/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# set -e
+
+PWD=$(pwd)
+
+# https://stackoverflow.com/a/246128
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_ROOT=$(realpath "$SCRIPT_PATH/../")
+
+source "$SCRIPT_PATH/helpers.sh"
+
+AT_LEAST_ONE_ERROR=0
+
+cd $PROJECT_ROOT
+capture_stdout_and_stderr_if_successful pytest
+capture_stdout_and_stderr_if_successful black --check .
+capture_stdout_and_stderr_if_successful isort --recursive -c .
+capture_stdout_and_stderr_if_successful mypy .
+cd $PWD
+
+echo $AT_LEAST_ONE_ERROR
+
+exit $AT_LEAST_ONE_ERROR

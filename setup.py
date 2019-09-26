8f59b59237596f08b40c530dd9e266a25ed9da77
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-09-26 17:51:58 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reactivated",
-    version="0.1.2",
+    version="0.2.0",
     author="Silvio Gutierrez",
     author_email="silviogutierrez@gmail.com",
     description="A statically typed framework to create Django sites with a React frontend.",

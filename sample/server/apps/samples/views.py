dc8c18b41fef397a7029d4313af17a25dc2c39f3
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-10-29 21:25:59 -0400

    Add tests and examples of multiple model picks

--- sample/server/apps/samples/views.py
+++ sample/server/apps/samples/views.py
@@ -86,7 +86,9 @@ def typed_template(
     assert opera is not None
     assert composer is not None
 
-    return templates.TypedTemplate(opera=opera, composer=composer).render(request)
+    return templates.TypedTemplate(
+        opera=opera, composer=composer, all_operas=list(models.Opera.objects.all())
+    ).render(request)
 
 
 @autocomplete

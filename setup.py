6b2686a296630b571eb69f1ff6c0e9322ee3d611
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2020-04-20 13:01:21 -0400

    Release 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reactivated",
-    version="0.5.0",
+    version="0.5.1",
     author="Silvio Gutierrez",
     author_email="silviogutierrez@gmail.com",
     description="A statically typed framework to create Django sites with a React frontend.",

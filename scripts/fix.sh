a176715557158f2357cacecdae4ab4b7da0277e5
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2020-06-23 22:26:02 -0400

    Improve linting, formatting and CI

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -1,19 +1,95 @@
 #!/bin/bash
-# set -e
+
+set -e
 
 PWD=$(pwd)
 
 # https://stackoverflow.com/a/246128
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PROJECT_ROOT=$(realpath "$SCRIPT_PATH/../")
 
+SPECIFIC_FILE=$1
 
-cd $PROJECT_ROOT
-autoflake  -i -r  --remove-all-unused-imports .
-isort --recursive .
-black .
-node_modules/.bin/tslint -p packages/reactivated --fix
-node_modules/.bin/tslint -p sample --fix
-node_modules/.bin/prettier --ignore-path .gitignore --check '**/*.{ts,tsx,yaml,json}' --write
+if [ -z "$SPECIFIC_FILE" ]; then
+    if [ "$GITHUB_BASE_REF" != "" ]; then
+        TARGET_BRANCH="origin/$GITHUB_BASE_REF"
+        echo "PR event: running against base branch $TARGET_BRANCH"
+    elif [ "$GITHUB_EVENT_NAME" = "push" ]; then
+        TARGET_BRANCH=$(git rev-parse HEAD~1)
+        echo "Push event: running against previous commit $TARGET_BRANCH"
+    else
+        TARGET_BRANCH="origin/master"
+        echo "Local run: running against default branch $TARGET_BRANCH"
+    fi
 
-cd $PWD
+    # Changed files against target branch, but exclude deleted files.
+    CHANGED_FILES=$(git diff --name-only --diff-filter d --relative "$(git merge-base $TARGET_BRANCH HEAD)")
+    CHANGED_FILES=${CHANGED_FILES// /}
+elif [ -f "$SPECIFIC_FILE" ]; then
+    echo "Specific file run: running against $SPECIFIC_FILE"
+    CHANGED_FILES=$SPECIFIC_FILE
+else
+    echo "Invalid file $SPECIFIC_FILE"
+    exit 1
+fi
+
+CHANGED_PY_FILES=$(echo "$CHANGED_FILES" | grep -e '.pyi\?$' || true)
+CHANGED_PRETTIER_FILES=$(echo "$CHANGED_FILES" | grep -e '.tsx\?$\|.yaml$\|.json$' || true)
+CHANGED_TSLINT_FILES=$(echo "$CHANGED_FILES" | grep -e '.tsx\?$' || true)
+CHANGED_SH_FILES=$(echo "$CHANGED_FILES" | grep -e '.sh$' || true)
+CHANGED_NIX_FILES=$(echo "$CHANGED_FILES" | grep -e '.nix$' || true)
+CHANGED_TF_FILES=$(echo "$CHANGED_FILES" | grep -e '.tf$\|.tfvars$' || true)
+
+echo -e "[Python]:\n$CHANGED_PY_FILES\n"
+echo -e "[Prettier]:\n$CHANGED_PRETTIER_FILES\n"
+echo -e "[Shell]:\n$CHANGED_SH_FILES\n"
+echo -e "[Nix]:\n${CHANGED_NIX_FILES}\n"
+echo -e "[Terraform]:\n${CHANGED_TF_FILES}\n"
+echo -e "[TSLint]:\n${CHANGED_TSLINT_FILES}\n"
+
+cd "$PROJECT_ROOT"
+
+if [[ -n "${CHANGED_PY_FILES// /}" ]]; then
+    # shellcheck disable=SC2086
+    autoflake --exclude node_modules,.venv -i -r --remove-all-unused-imports $CHANGED_PY_FILES
+
+    # shellcheck disable=SC2086
+    isort --recursive $CHANGED_PY_FILES
+
+    # shellcheck disable=SC2086
+    black $CHANGED_PY_FILES
+fi
+
+if [[ -n "${CHANGED_PRETTIER_FILES// /}" ]]; then
+    # shellcheck disable=SC2086
+    node_modules/.bin/prettier --ignore-path .gitignore $CHANGED_PRETTIER_FILES --write
+fi
+
+if [[ -n "${CHANGED_SH_FILES// /}" ]]; then
+    # shellcheck disable=SC2086
+    SHELLCHECK_DIFF=$(shellcheck -x -f diff $CHANGED_SH_FILES || true)
+
+    if [[ -n "${SHELLCHECK_DIFF// /}" ]]; then
+        echo "$SHELLCHECK_DIFF" | git apply
+    fi
+
+    # shellcheck disable=SC2086
+    shfmt -w $CHANGED_SH_FILES
+fi
+
+if [[ -n "${CHANGED_NIX_FILES// /}" ]]; then
+    # shellcheck disable=SC2086
+    nixfmt $CHANGED_NIX_FILES
+fi
+
+if [[ -n "${CHANGED_TF_FILES// /}" ]]; then
+    # shellcheck disable=SC2086
+    terraform fmt $CHANGED_TF_FILES
+fi
+
+if [[ -n "${CHANGED_TSLINT_FILES// /}" ]]; then
+    # shellcheck disable=SC2086
+    node_modules/.bin/tslint -p . --fix $CHANGED_TSLINT_FILES
+fi
+
+cd "$PWD"

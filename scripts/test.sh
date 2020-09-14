23569a30ebc557f354d787ab200774eb8755e698
Silvio <silviogutierrez@gmail.com>
2020-09-14 09:38:33 -0400

    Add Nix for local development (#41)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,24 +1,106 @@
 #!/bin/bash
-# set -e
+
+set -e
 
 PWD=$(pwd)
 
-# https://stackoverflow.com/a/246128
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT_ROOT=$(realpath "$SCRIPT_PATH/../")
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PROJECT_ROOT=$(realpath "$SCRIPT_PATH/..")
 
+if [ "$GITHUB_BASE_REF" != "" ]; then
+    TARGET_BRANCH="origin/$GITHUB_BASE_REF"
+    echo "PR event: running against base branch $TARGET_BRANCH"
+elif [ "$GITHUB_EVENT_NAME" = "push" ]; then
+    TARGET_BRANCH=$(git rev-parse HEAD~1)
+    echo "Push event: running against previous commit $TARGET_BRANCH"
+else
+    TARGET_BRANCH="origin/master"
+    echo "Local run: running against default branch $TARGET_BRANCH"
+fi
+
+SERVER=1
+CLIENT=1
+E2E=1
+INFRASTRUCTURE=1
+
+# shellcheck source=SCRIPTDIR/helpers.sh
 source "$SCRIPT_PATH/helpers.sh"
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    --server)
+        SERVER=1
+        CLIENT=0
+        E2E=0
+        INFRASTRUCTURE=0
+        shift
+        ;;
+    --client)
+        SERVER=0
+        CLIENT=1
+        E2E=0
+        INFRASTRUCTURE=0
+        shift
+        ;;
+    --e2e)
+        SERVER=0
+        CLIENT=0
+        E2E=1
+        INFRASTRUCTURE=0
+        shift
+        ;;
+    --infrastructure)
+        SERVER=0
+        CLIENT=0
+        E2E=0
+        INFRASTRUCTURE=1
+        shift
+        ;;
+    *) usage ;;
+    esac
+done
 
 AT_LEAST_ONE_ERROR=0
 
 cd $PROJECT_ROOT
-capture_stdout_and_stderr_if_successful pytest
-capture_stdout_and_stderr_if_successful black --check .
-capture_stdout_and_stderr_if_successful isort --recursive -c .
-capture_stdout_and_stderr_if_successful mypy --no-incremental .
-capture_stdout_and_stderr_if_successful node_modules/.bin/tslint -p packages/reactivated
-capture_stdout_and_stderr_if_successful node_modules/.bin/tslint -p sample
-capture_stdout_and_stderr_if_successful node_modules/.bin/prettier --ignore-path .gitignore --check '**/*.{ts,tsx,yaml,json}'
-cd $PWD
+
+if [[ $SERVER -eq 1 ]]; then
+    capture_stdout_and_stderr_if_successful pytest
+    capture_stdout_and_stderr_if_successful flake8 .
+    capture_stdout_and_stderr_if_successful isort --recursive -c .
+    capture_stdout_and_stderr_if_successful black . --check
+    capture_stdout_and_stderr_if_successful mypy --no-incremental .
+fi
+
+if [[ $CLIENT -eq 1 ]]; then
+    capture_stdout_and_stderr_if_successful node_modules/.bin/tslint -p packages/reactivated
+    capture_stdout_and_stderr_if_successful node_modules/.bin/tslint -p sample
+    capture_stdout_and_stderr_if_successful node_modules/.bin/prettier --ignore-path .gitignore --check '**/*.{ts,tsx,yaml,json}'
+fi
+
+if [[ $E2E -eq 1 ]]; then
+    capture_stdout_and_stderr_if_successful echo "No E2E tests yet"
+fi
+
+if [[ $INFRASTRUCTURE -eq 1 ]]; then
+    CHANGED_NIX_FILES=$(git diff --name-only --diff-filter d --relative "$(git merge-base $TARGET_BRANCH HEAD)" | grep -e '.nix$' || true)
+    CHANGED_SH_FILES=$(git diff --name-only --diff-filter d --relative "$(git merge-base $TARGET_BRANCH HEAD)" | grep -e '.sh$' || true)
+
+    if [[ -n "${CHANGED_SH_FILES// /}" ]]; then
+        # shellcheck disable=SC2086
+        capture_stdout_and_stderr_if_successful shellcheck $CHANGED_SH_FILES -x
+
+        # shellcheck disable=SC2086
+        capture_stdout_and_stderr_if_successful shfmt -d $CHANGED_SH_FILES
+    fi
+
+    if [[ -n "${CHANGED_NIX_FILES// /}" ]]; then
+        # shellcheck disable=SC2086
+        capture_stdout_and_stderr_if_successful nixfmt -c $CHANGED_NIX_FILES
+    fi
+
+    capture_stdout_and_stderr_if_successful terraform fmt -recursive -check
+fi
+
+cd "$PWD"
 
 exit $AT_LEAST_ONE_ERROR
---
+++ shell.nix
@@ -0,0 +1,39 @@
+let
+  stableTarball = fetchTarball
+    "https://github.com/NixOS/nixpkgs-channels/archive/0a146054bdf6f70f66de4426f84c9358521be31e.tar.gz";
+  unstableTarball = fetchTarball
+    "https://github.com/NixOS/nixpkgs-channels/archive/0a146054bdf6f70f66de4426f84c9358521be31e.tar.gz";
+  pkgs = import stableTarball { };
+  unstable = import unstableTarball { };
+in with pkgs;
+
+mkShell {
+  buildInputs = [
+    gitAndTools.gh
+
+    python38
+    python38Packages.virtualenv
+    nodejs-13_x
+    (yarn.override { nodejs = nodejs-13_x; })
+
+    # Otherwise AWS gets some weird conflicts with python 2.7
+    (tmuxp.override { python = python38; })
+
+    ripgrep
+    shellcheck
+    shfmt
+    nixfmt
+
+    # Purely for formatting right now.
+    terraform
+  ];
+  src = ./scripts/helpers.sh;
+  shellHook = ''
+    # Needed to use pip wheels
+    SOURCE_DATE_EPOCH=$(date +%s);
+
+    source $src;
+
+    setup_environment;
+  '';
+}

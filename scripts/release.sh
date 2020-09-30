1cea309829d46b3fcf44f2ae2b5693811ba978cb
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2020-09-30 16:19:07 -0400

    Add job to create releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,29 +2,45 @@
 
 set -e
 
-CURRENT_VERSION=$(
-    cat <<EOF | python
-import json
-with open("packages/reactivated/package.json", "r") as package:
-    version = json.load(package)["version"]
-    print(version)
-EOF
-)
+IS_SNAPSHOT=false
+VERSIONING="minor"
 
-# Need jq, migrate to nix to fix this.
-# CURRENT_VERSION=$(jq <packages/reactivated/package.json .version -r)
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    --versioning)
+        VERSIONING="$2"
+        shift
+        ;;
+    --snapshot) IS_SNAPSHOT=true ;;
+    *)
+        echo "Unknown parameter passed: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
 
 echo "Current version: $CURRENT_VERSION"
-SNAPSHOT_VERSION="${CURRENT_VERSION}a${GITHUB_RUN_NUMBER}"
-echo "Snapshot version: $SNAPSHOT_VERSION"
 
 cd packages/reactivated/
-yarn version --no-git-tag-version --new-version "${CURRENT_VERSION}a${GITHUB_RUN_NUMBER}"
-yarn publish --tag cd
-echo "Published version $SNAPSHOT_VERSION to NPM"
+
+CURRENT_VERSION=$(jq <package.json .version -r)
+
+if [ "$IS_SNAPSHOT" = false ]; then
+    yarn version "--$VERSIONING"
+    echo "Release version: $NEW_VERSION"
+    NEW_VERSION=$(jq <package.json .version -r)
+    yarn publish
+else
+    NEW_VERSION="${CURRENT_VERSION}a${GITHUB_RUN_NUMBER}"
+    echo "Snapshot version: $NEW_VERSION"
+    yarn version --no-git-tag-version --new-version "${NEW_VERSION}"
+    yarn publish --tag cd
+fi
+echo "Published version $NEW_VERSION to NPM"
 cd -
 
 pip install wheel
 python setup.py sdist bdist_wheel
 twine upload dist/*
-echo "Published version $SNAPSHOT_VERSION to PyPI"
+echo "Published version $NEW_VERSION to PyPI"

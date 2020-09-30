f9595c2534a63e07cc852a1c9ae3ec548d78e186
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2020-09-30 19:23:08 -0400

    Last fixes.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,36 @@
 
 set -e
 
+REPOSITORY_URL="https://api.github.com/repos/silviogutierrez/reactivated/branches/master/protection/required_status_checks"
+
+function disable_github_checks() {
+    # Disable branch protection
+    EXISTING_CHECKS=$(curl --url $REPOSITORY_URL \
+        --header "Authorization: Bearer $GITHUB_TOKEN" \
+        --header 'Content-Type: application/json' |
+        jq ". | {strict, contexts}" -r)
+
+    curl -X PATCH --url $REPOSITORY_URL \
+        --header "Authorization: Bearer $GITHUB_TOKEN" \
+        --header 'Content-Type: application/json' \
+        --data-binary @- <<EOF
+    {
+      "strict": false,
+      "contexts": []
+    }
+EOF
+    echo "$EXISTING_CHECKS"
+}
+
+function enable_github_checks() {
+    CHECKS_TO_ENABLE=$1
+    # Enable branch protection
+    curl -X PATCH --url $REPOSITORY_URL \
+        --header "Authorization: Bearer $GITHUB_TOKEN" \
+        --header 'Content-Type: application/json' \
+        --data-binary "$CHECKS_TO_ENABLE"
+}
+
 IS_SNAPSHOT=false
 VERSIONING="minor"
 
@@ -26,48 +56,28 @@ cd packages/reactivated/
 
 CURRENT_VERSION=$(jq <package.json .version -r)
 
-URL="https://api.github.com/repos/silviogutierrez/reactivated/branches/master/protection/required_status_checks"
-
-# Disable branch protection
-EXISTING_RAW=$(curl --url $URL \
- --header "Authorization: Bearer $GITHUB_TOKEN" \
- --header 'Content-Type: application/json');
-
-EXISTING=$(echo "$EXISTING_RAW" | jq ". | {strict, contexts}" -r)
-
-curl -X PATCH --url $URL \
- --header "Authorization: Bearer $GITHUB_TOKEN" \
- --header 'Content-Type: application/json' \
- --data-binary @- <<EOF
-{
-  "strict": false,
-  "contexts": []
-}
-EOF
-
 if [ "$IS_SNAPSHOT" = false ]; then
     yarn version "--$VERSIONING"
     echo "Release version: $NEW_VERSION"
     NEW_VERSION=$(jq <package.json .version -r)
     yarn publish
+
+    EXISTING_CHECKS=$(disable_github_checks)
     git push
     git push --tags
+    enable_github_checks "$EXISTING_CHECKS"
 else
     NEW_VERSION="${CURRENT_VERSION}a${GITHUB_RUN_NUMBER}"
     echo "Snapshot version: $NEW_VERSION"
+    EXISTING_CHECKS=$(disable_github_checks)
     yarn version --no-git-tag-version --new-version "${NEW_VERSION}"
     # yarn publish --tag cd
+    enable_github_checks "$EXISTING_CHECKS"
 fi
 echo "Published version $NEW_VERSION to NPM"
 cd -
 
-# Enable branch protection
-curl -X PATCH --url $URL \
- --header "Authorization: Bearer $GITHUB_TOKEN" \
- --header 'Content-Type: application/json' \
- --data-binary "$EXISTING"
-
 pip install wheel
-# python setup.py sdist bdist_wheel
-twine upload dist/*
+python setup.py sdist bdist_wheel
+# twine upload dist/*
 echo "Published version $NEW_VERSION to PyPI"

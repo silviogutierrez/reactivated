5e256a7207284cbfa99df13658fb1814280eba27
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2020-11-28 17:04:38 -0500

    Revert

--- sample/client/templates/hello_world.tsx
+++ sample/client/templates/hello_world.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Context from "reactivated/context";
 import {style} from "typestyle";
 
 const styles = {
@@ -8,41 +7,10 @@ const styles = {
     header: style({color: "blue"}),
 } as const;
 
-export const ServerData = () => {
-    const context = React.useContext(Context);
-    const props = (global as any).__REACTIVATED_PROPS ?? null;
-    const serializedContext = JSON.stringify(context).replace(/</g, "\\u003c");
-    const serializedProps = JSON.stringify(props).replace(/</g, "\\u003c");
-
-    return (
-        <>
-            <meta name="reactivated-context" content={serializedContext} />
-            <meta
-                name="reactivated-props"
-                suppressHydrationWarning
-                content={serializedProps}
-            />
-        </>
-    );
-};
-
 export default () => (
-    <>
-        <head>
-            <ServerData />
-            <title>Hello world</title>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <script defer src="/media/dist/bundle.js" />
-        </head>
-        <body>
-            <div className={styles.layout}>
-                <h1 className={styles.header}>Hello world!</h1>
-                <p>I am a paragraph</p>
-                <p>I am another paragraph</p>
-            </div>
-        </body>
-    </>
+    <div className={styles.layout}>
+        <h1 className={styles.header}>Hello world!</h1>
+        <p>I am a paragraph</p>
+        <p>I am another paragraph</p>
+    </div>
 );

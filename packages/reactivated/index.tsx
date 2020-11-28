0e68c15a7f35277c7ade6a7e1dc08246e2ebcf99
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2020-11-28 13:42:22 -0500

    Switch to async

--- packages/reactivated/index.tsx
+++ packages/reactivated/index.tsx
@@ -7,55 +7,9 @@ export * from "./components/Widget";
 
 import Context from "./context";
 
-export const ServerData = () => {
-    const props = (global as any).__REACTIVATED_PROPS ?? null;
-    const context = React.useContext(Context);
-
-    let serializedContext = "";
-    let serializedProps = "";
-
-    // Only execute on the server, where props won't be null.
-    if (props != null) {
-        // TODO: redux docs have this replacing, but since we're encoding, is it still really necessary?
-        // See: https://redux.js.org/recipes/server-rendering#inject-initial-component-html-and-state
-        serializedContext = Buffer.from(
-            JSON.stringify(context).replace(/</g, "\\u003c"),
-        ).toString("base64");
-        serializedProps = Buffer.from(
-            JSON.stringify(props).replace(/</g, "\\u003c"),
-        ).toString("base64");
-    }
-
-    // We suppress hydration warnings because the client content will always
-    // be an empty string.
-    return (
-        <>
-            <meta
-                name="reactivated-context"
-                suppressHydrationWarning
-                content={serializedContext}
-            />
-            <meta
-                name="reactivated-props"
-                suppressHydrationWarning
-                content={serializedProps}
-            />
-        </>
-    );
-};
-
 export const getServerData = <T extends {}>() => {
-    const props = JSON.parse(
-        atob(
-            (document.getElementsByName("reactivated-props")[0] as HTMLMetaElement).content,
-        ),
-    );
-    const context: T = JSON.parse(
-        atob(
-            (document.getElementsByName("reactivated-context")[0] as HTMLMetaElement)
-                .content,
-        ),
-    );
+    const props = (window as any).__PRELOADED_PROPS__;
+    const context: T = (window as any).__PRELOADED_CONTEXT__;
 
     return {props, context};
-}
+};
--- packages/reactivated/server.tsx
+++ packages/reactivated/server.tsx
@@ -5,7 +5,7 @@ import {compile} from "json-schema-to-typescript";
 import path from "path";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
-import Helmet, {HelmetData} from "react-helmet";
+import {FilledContext, Helmet, HelmetData, HelmetProvider} from "react-helmet-async";
 import webpack from "webpack";
 
 import moduleAlias from "module-alias";
@@ -23,15 +23,43 @@ const app = express();
 
 export const bindRenderPage = (settings: Settings) => ({
     html,
+    helmet,
     context,
     props,
 }: {
     html: string;
+    helmet: any;
     context: any;
     props: any;
-}) => `<!DOCTYPE html>
+}) => `
+<!DOCTYPE html>
 <html>
-    ${html}
+    <head ${helmet.htmlAttributes.toString()}>
+        <script>
+            // These go first because scripts below need them.
+            // WARNING: See the following for security issues around embedding JSON in HTML:
+            // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+            window.__PRELOADED_PROPS__ = ${JSON.stringify(props).replace(
+                /</g,
+                "\\u003c",
+            )}
+            window.__PRELOADED_CONTEXT__ = ${JSON.stringify(context).replace(
+                /</g,
+                "\\u003c",
+            )}
+        </script>
+
+        ${helmet.base.toString()}
+        ${helmet.link.toString()}
+        ${helmet.meta.toString()}
+        ${helmet.noscript.toString()}
+        ${helmet.script.toString()}
+        ${helmet.style.toString()}
+        ${helmet.title.toString()}
+    </head>
+    <body ${helmet.bodyAttributes.toString()}>
+        <div id="root">${html}</div>
+    </body>
 </html>
 `;
 
@@ -94,19 +122,24 @@ export const render = (
         // }
     }
 
+    const helmetContext = {} as FilledContext;
     const Template = require(templatePath).default;
-    (global as any).__REACTIVATED_PROPS = props;
     // See https://github.com/downshift-js/downshift#resetidcounter
     resetIdCounter();
 
     const rendered = ReactDOMServer.renderToString(
-        <Provider value={context}>
-            <Template {...props} />
-        </Provider>,
+        <HelmetProvider context={helmetContext}>
+            <Provider value={context}>
+                <div>Helo</div>
+            </Provider>
+        </HelmetProvider>,
     );
+
+    const {helmet} = helmetContext;
 
     return renderPage({
         html: rendered,
+        helmet,
         props,
         context,
     });

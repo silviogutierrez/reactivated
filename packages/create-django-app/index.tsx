c0cd56f571a5120bc5fc817b7ce7b9e1f0cee58a
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2021-03-18 09:36:52 -0400

    Initial files with script for nix

---
+++ packages/create-django-app/index.tsx
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+import child_process from "child_process";
+
+const cleanup = () => {
+    console.log("Cleaning up.");
+};
+
+const handleExit = () => {
+    cleanup();
+    console.log("Exiting without error.");
+    process.exit();
+};
+
+const handleError = (e: unknown) => {
+    console.error("ERROR! An error was encountered while executing");
+    console.error(e);
+    cleanup();
+    console.log("Exiting with error.");
+    process.exit(1);
+};
+
+process.on("SIGINT", handleExit);
+process.on("uncaughtException", handleError);
+
+try {
+    const nixStatus = child_process.execSync(`nix`, {stdio: "ignore"});
+} catch (error) {
+    console.log(
+        "You need to install nix. Visit https://nixos.org/download.html to get started",
+    );
+    throw error;
+}

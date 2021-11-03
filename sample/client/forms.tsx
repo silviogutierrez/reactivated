7b4be4a3a2f8efcb0a96ad14f2c5054e57bcfb05
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2021-11-02 23:53:42 -0400

    Basic widget extracted

--- sample/client/forms.tsx
+++ sample/client/forms.tsx
@@ -42,8 +42,6 @@ export interface FormLike<T extends FieldMap> {
 
 type Initializers = {[K in Widget_GENERATEME["tag"]]: (widget: DiscriminateUnion<Widget_GENERATEME, "tag", K>) => DiscriminateUnion<Widget_GENERATEME, "tag", K>["value_from_datadict"]}
 
-type WidgetValues = {[K in Widget_GENERATEME["tag"]]: {tag: K, value: DiscriminateUnion<Widget_GENERATEME, "tag", K>["value_from_datadict"]}}[Widget_GENERATEME["tag"]];
-
 const initializers: Initializers = {
     "django.forms.widgets.CheckboxInput": (widget) => {
         if (widget.attrs.checked === true) {
@@ -82,6 +80,17 @@ const useInitialState = <T extends FieldMap>(form: FormLike<T>) => {
     >;
 }
 
+
+type OuterTagged = {
+    [K in Widget_GENERATEME["tag"]]: {
+        name: string;
+        tag: K;
+        value: DiscriminateUnion<Widget_GENERATEME, "tag", K>["value_from_datadict"];
+        handler: (value: DiscriminateUnion<Widget_GENERATEME, "tag", K>["value_from_datadict"]) => void;
+        widget: DiscriminateUnion<Widget_GENERATEME, "tag", K>,
+    };
+}[Widget_GENERATEME["tag"]];
+
 const useForm = <T extends FieldMap>({form}: {form: FormLike<T>}) => {
     const initialState = useInitialState(form);
     const [values, setValues] = React.useState(initialState);
@@ -117,7 +126,7 @@ const useForm = <T extends FieldMap>({form}: {form: FormLike<T>}) => {
         };
     }[Widget_GENERATEME["tag"]];
 
-    const iterate = (callback: (field: Tagged, thing: WidgetValues) => void) => {
+    const iterate = (callback: (field: Tagged) => void) => {
         return form.iterator.map((fieldName) => {
             const field = form.fields[fieldName];
             const handler: any = (handlers[(field.widget as any).tag as keyof typeof handlers] ?? defaultHandler)(fieldName);
@@ -128,7 +137,7 @@ const useForm = <T extends FieldMap>({form}: {form: FormLike<T>}) => {
                 value: values[fieldName],
                 handler,
                 widget: field.widget as any,
-            }, null as any)
+            })
         });
     }
 
@@ -170,60 +179,72 @@ const Select = (props: {name: string, value: string | number | null, optgroups: 
     </select>
 }
 
+export const Widget = (props: {field: OuterTagged}) => {
+    const {field} = props;
+
+    if (field.tag === "django.forms.widgets.CheckboxInput") {
+        return <CheckboxInput key={field.name} name={field.name} value={field.value} onChange={field.handler} />
+    }
+    else if (field.tag === "django.forms.widgets.TextInput") {
+        return (
+            <TextInput
+                key={field.name}
+                name={field.name}
+                value={field.value}
+                onChange={field.handler}
+            />
+        );
+    } else if (field.tag === "django.forms.widgets.SelectDateWidget") {
+
+        return (
+            <React.Fragment key={field.name}>
+                {field.widget.subwidgets.map((subwidget) => {
+                    const unprefixedName = subwidget.name.replace(`${field.name}_`, "") as keyof typeof field.value;
+
+                    return <Widget key={subwidget.name} field={{
+                        ...field,
+                        tag: "django.forms.widgets.Select",
+                        widget: subwidget,
+                    } as any} />
+                    /*
+                    return (
+                        <Select
+                            key={subwidget.name}
+                            name={subwidget.name}
+                            value={field.value[unprefixedName]}
+                            optgroups={subwidget.optgroups}
+                            onChange={(value) => {
+                                field.handler(
+                                    {
+                                        ...field.value,
+                                        [unprefixedName]: value,
+                                    }
+                                );
+                            }}
+                        />
+                    );
+                    */
+                })}
+            </React.Fragment>
+        );
+    } else if (field.tag === "django.forms.widgets.Select") {
+        return (
+            <Select
+                key={field.name}
+                name={field.name}
+                value={field.value}
+                optgroups={field.widget.optgroups}
+                onChange={field.handler}
+            />
+        );
+    }
+    throw new Error("Exhaustive");
+}
+
 export const Form = <T extends FieldMap>(props: {form: FormLike<T>}) => {
     const form = useForm(props);
 
-    const rendered = form.iterate((field, thing) => {
-        if (field.tag === "django.forms.widgets.CheckboxInput") {
-            return <CheckboxInput key={field.name} name={field.name} value={field.value} onChange={field.handler} />
-        }
-        else if (field.tag === "django.forms.widgets.TextInput") {
-            return (
-                <TextInput
-                    key={field.name}
-                    name={field.name}
-                    value={field.value}
-                    onChange={field.handler}
-                />
-            );
-        } else if (field.tag === "django.forms.widgets.SelectDateWidget") {
-
-            return (
-                <React.Fragment key={field.name}>
-                    {field.widget.subwidgets.map((subwidget) => {
-                        const unprefixedName = subwidget.name.replace(`${field.name}_`, "") as keyof typeof field.value;
-
-                        return (
-                            <Select
-                                key={subwidget.name}
-                                name={subwidget.name}
-                                value={field.value[unprefixedName]}
-                                optgroups={subwidget.optgroups}
-                                onChange={(value) => {
-                                    field.handler(
-                                        {
-                                            ...field.value,
-                                            [unprefixedName]: value,
-                                        }
-                                    );
-                                }}
-                            />
-                        );
-                    })}
-                </React.Fragment>
-            );
-        } else if (field.tag === "django.forms.widgets.Select") {
-            return (
-                <Select
-                    key={field.name}
-                    name={field.name}
-                    value={field.value}
-                    optgroups={field.widget.optgroups}
-                    onChange={field.handler}
-                />
-            );
-        }
-    });
+    const rendered = form.iterate((field) => <Widget field={field} />);
 
     return <div>
         {rendered}

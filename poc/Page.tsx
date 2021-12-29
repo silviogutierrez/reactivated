9e4cab4bad31bc3095a40d06a0ad4dff1fd0adb8
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2021-12-28 23:23:33 -0500

    This is working

---
+++ poc/Page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Page = () => <div>
+    <h1>My Page</h1>
+</div>
---
+++ poc/index.tsx
@@ -0,0 +1,64 @@
+import http from "http";
+import {Page} from "./Page";
+import ReactDOMServer from "react-dom/server";
+import React from "react";
+
+const OK_RESPONSE = 200;
+
+const ERROR_REPONSE = 500;
+
+const PORT = 3000;
+
+type Rendered = {
+    status: "success",
+    rendered: string;
+} | {
+    status: "error",
+    error: unknown,
+}
+
+const render = (body: Buffer): Rendered => {
+    const rendered = ReactDOMServer.renderToString(
+        <Page />
+    );
+
+    return {
+        status: "success",
+        rendered,
+    }
+}
+
+const server = http.createServer((req, res) => {
+    let body = Buffer.from("");
+
+    req.on("data", (chunk) => {
+        body = Buffer.concat([body, chunk as Buffer]);
+    });
+    req.on("end", () => {
+        const result = render(body);
+
+        if (result.status === "success") {
+            res.writeHead(OK_RESPONSE, {"Content-Type": "text/html; charset=utf-8"});
+            res.end(result.rendered);
+        } else {
+            res.writeHead(ERROR_REPONSE, {"Content-Type": "application/json"});
+            res.end(
+                JSON.stringify(result.error, Object.getOwnPropertyNames(result.error)),
+            );
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    const address = server.address();
+
+    if (typeof address === "string") {
+        throw new Error();
+    }
+    process.stdout.write(`RENDERER:${address.port.toString()}:LISTENING`);
+
+    // TODO: load this from a passed in parameter.
+    // const warmUpTemplate = "HomePage";
+    // const templatePath = `${process.cwd()}/client/templates/${warmUpTemplate}`;
+    // require(templatePath);
+});

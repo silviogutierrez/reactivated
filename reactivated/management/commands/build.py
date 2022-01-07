6cb989432cba1d9d9796c6c86393e9a5c1f7786d
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-01-06 22:59:26 -0500

    Add type checking

--- reactivated/management/commands/build.py
+++ reactivated/management/commands/build.py
@@ -3,7 +3,7 @@ import subprocess
 from typing import Any
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
@@ -17,16 +17,28 @@ class Command(BaseCommand):
             "NODE_ENV": "production",
         }
 
+        tsc_process = subprocess.Popen(
+            ["./node_modules/.bin/tsc", "--noEmit"],
+            stdout=subprocess.PIPE,
+            env=build_env,
+        )
         client_process = subprocess.Popen(
             ["node", "./node_modules/reactivated/build.client.js", *entry_points],
             stdout=subprocess.PIPE,
             env=build_env,
         )
-        client_process.wait()
         renderer_process = subprocess.Popen(
             ["node", "./node_modules/reactivated/build.renderer.js"],
             stdout=subprocess.PIPE,
             env=build_env,
         )
+
+        tsc_output, tsc_error = tsc_process.communicate()
         client_output, client_error = client_process.communicate()
         renderer_ouput, renderer_error = renderer_process.communicate()
+
+        if tsc_process.returncode != 0:
+            raise CommandError("TypeScript error. Run 'tsc --noEmit' manually.")
+
+        if client_error.returncode != 0  or renderer_error.returncode != 0:
+            raise CommandError("esbuild errors")

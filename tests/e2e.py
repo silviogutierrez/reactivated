8957b78fc362f96bde938e6591cd668574e871d8
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-01-12 09:05:30 -0500

    Bring test back

--- tests/e2e.py
+++ tests/e2e.py
@@ -7,7 +7,7 @@ from reactivated import registry
 os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
 
 
-def test_end_to_end(client, live_server):
+def test_end_to_end(client, live_server, page):
     registry.type_registry.clear()
     registry.global_types.clear()
     registry.template_registry.clear()
@@ -17,11 +17,10 @@ def test_end_to_end(client, live_server):
     os.chdir("sample")
     call_command("generate_client_assets")
     call_command("build")
-    return
 
-    # if os.path.exists("./node_modules/.bin/reactivated.sock"):
-    #     os.remove("./node_modules/.bin/reactivated.sock")
+    if os.path.exists("./node_modules/.bin/reactivated.sock"):
+        os.remove("./node_modules/.bin/reactivated.sock")
 
-    # page.goto(live_server.url)
-    # assert "<h1>Hello World!</h1>" in page.content()
+    page.goto(live_server.url)
+    assert "<h1>Hello World!</h1>" in page.content()
     os.chdir("../")

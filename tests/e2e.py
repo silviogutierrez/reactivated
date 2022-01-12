d164428b45c9917d04ce63f1ecfa2c8b9bbf2af6
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-01-12 01:07:09 -0500

    Try it all

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,17 +1,26 @@
 import os
 
 from django.core.management import call_command
-from django.urls import reverse
+
+from reactivated import registry
+
+os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
 
 
-def test_end_to_end(client, live_server):
+def test_end_to_end(client, live_server, page):
+    registry.type_registry.clear()
+    registry.global_types.clear()
+    registry.template_registry.clear()
+    registry.value_registry.clear()
+    registry.definitions_registry.clear()
+
     os.chdir("sample")
+    call_command("generate_client_assets")
     call_command("build")
 
     if os.path.exists("./node_modules/.bin/reactivated.sock"):
         os.remove("./node_modules/.bin/reactivated.sock")
 
-    url = reverse("home_page")
-    response = client.get(url)
-    assert "<h1>Hello World!</h1>" in response.rendered_content
+    page.goto(live_server.url)
+    assert "<h1>Hello World!</h1>" in page.content()
     os.chdir("../")

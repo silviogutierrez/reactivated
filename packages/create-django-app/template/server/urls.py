be5e1e230ce98bb781b67f4505383666e911e0c2
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-01-19 21:27:44 -0500

    Now we are talking

--- packages/create-django-app/template/server/urls.py
+++ packages/create-django-app/template/server/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("example.urls")),
+    path("", include("server.example.urls")),
 ]

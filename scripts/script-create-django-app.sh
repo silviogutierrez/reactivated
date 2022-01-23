146dca13adb8ee8a60748260ad705d728573ef98
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-01-22 20:52:04 -0500

    better

--- scripts/script-create-django-app.sh
+++ scripts/script-create-django-app.sh
@@ -7,8 +7,11 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 PROJECT_NAME="testproject"
 
+# TODO: this should go away once we use workspaces, etc.
+nix-shell --command "yarn --cwd packages/reactivated"
 nix-shell --command "rm -rf $PROJECT_NAME/node_modules/reactivated/* && yarn --cwd packages/reactivated tsc --outDir ../../$PROJECT_NAME/node_modules/reactivated"
 
 cd $PROJECT_NAME
 nix-shell --command "pip install -e $SCRIPT_PATH/../"
 # nix-shell --command "python manage.py runserver"
+nix-shell --command "python manage.py print_schema"

f8d30b84ff78dc50fa5f77a616ae47cda853e356
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-02-11 15:47:26 -0500

    Working sort of

---
+++ packages/create-django-app/default.nix
@@ -0,0 +1,19 @@
+appName:
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8ca77a63599e.tar.gz") { };
+  download = pkgs.fetchurl {url = "http://localhost:8000/static/script.sh"; executable = true; hash = null;};
+in with pkgs;
+
+mkShell {
+  buildInputs = [
+  ];
+  src = download;
+  appName = appName;
+  shellHook = ''
+    echo $appName;
+    exit
+    // echo "Enter a project name"
+    // read project_name
+    // $src $project_name;
+  '';
+}

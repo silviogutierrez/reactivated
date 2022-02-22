b36b320c6e1371140e511bfa8ad2a82daa21c03f
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-02-22 14:23:25 -0500

    Fix

--- development/scripts/remove_example.sh
+++ development/scripts/remove_example.sh
@@ -3,6 +3,6 @@
 sed -i /example/d server/settings/common.py
 sed -i /example/d server/urls.py
 rm -r server/example
-rm client/templates/DjangoDefault.tsx
+rm client/templates/{DjangoDefault,EditPoll,PollDetail,PollsIndex,Results}.tsx
 echo "DELETE from django_migrations WHERE app = 'example'" | python manage.py dbshell
 rm "$0"

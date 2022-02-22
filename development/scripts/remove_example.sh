2462a631ade8371cda65b56b894e045af293bd56
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-02-22 15:02:28 -0500

    Finally

--- development/scripts/remove_example.sh
+++ development/scripts/remove_example.sh
@@ -1,12 +1,13 @@
 #! /usr/bin/env nix-shell
-#! nix-shell ../shell.nix --pure -i bash
+#! nix-shell ../shell.nix -i bash
 DJANGO_VERSION=$(python -c "import django; print(django.get_version(), end='')")
 sed -i /example/d server/settings/common.py
 sed -i /example/d server/urls.py
-rm -r server/example
+git rm -r server/example
 # shellcheck disable=SC2016
 sed -i 's/${context.django_version}/'"$DJANGO_VERSION"'/' client/components/Layout.tsx
 sed -i 's/{context.django_version}/'"$DJANGO_VERSION"'/' client/components/Layout.tsx
-rm client/templates/{DjangoDefault,EditPoll,PollDetail,PollsIndex,Results}.tsx
+git rm client/templates/{DjangoDefault,EditPoll,PollDetail,PollsIndex,Results}.tsx
+scripts/fix.sh
 echo "DELETE from django_migrations WHERE app = 'example'" | python manage.py dbshell
-rm "$0"
+git rm -f "$0"

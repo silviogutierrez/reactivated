b59a556e88b549caeb865d04aa7fc94e769c5d4b
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-02-22 12:17:56 -0500

    better exit code

--- development/scripts/helpers.sh
+++ development/scripts/helpers.sh
@@ -7,7 +7,11 @@ function capture_stdout_and_stderr_if_successful() {
 
     echo -n "Running $*"
 
-    if ! OUTPUT=$("$@" 2>&1); then
+    OUTPUT=$("$@" 2>&1)
+    EXIT_CODE=$?
+
+    # pytest exist with 5 if no tests are found. We consider this successful.
+    if [ $EXIT_CODE -ne 0 ] && [ $EXIT_CODE -ne 5 ]; then
         # Used by the test script as a weird way to pass around failure. There's probably
         # a better way to do this.
         # shellcheck disable=SC2034

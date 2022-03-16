1596d6140ee0a09e9c6650082a4e4ecab7609e00
Silvio <silviogutierrez@gmail.com>
2022-03-16 15:50:06 -0400

    Add error when picking reverse relationships (#151)

--- reactivated/serialization/__init__.py
+++ reactivated/serialization/__init__.py
@@ -20,6 +20,7 @@ from typing import (
 from django import forms as django_forms
 from django.conf import settings
 from django.db import models
+from django.db.models.fields.reverse_related import ForeignObjectRel
 from django.utils.module_loading import import_string
 
 from reactivated import fields, stubs
@@ -781,6 +782,10 @@ def create_schema(Type: Any, definitions: Definitions) -> Thing:
         return Thing(schema={}, definitions=definitions)
     elif callable(getattr(Type, "get_json_schema", None)):
         return Type.get_json_schema(definitions)  # type: ignore[no-any-return]
+    elif isinstance(Type, ForeignObjectRel):
+        assert (
+            False
+        ), f"You cannot Pick reverse relationships. Specify which fields from {Type.name} you want, such as {Type.name}.example_field"
     elif issubclass(Type, tuple) and callable(getattr(Type, "_asdict", None)):
         return named_tuple_schema(Type, definitions)
     elif type(Type) == stubs._TypedDictMeta:

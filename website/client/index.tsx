858ebddebc3af6500451e9b93eb1f70eaf9c116f
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-04-11 15:34:07 -0400

    Tweaks

--- website/client/index.tsx
+++ website/client/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import {hydrate} from "react-dom";
 
 import {Provider, getServerData, getTemplate} from "@reactivated";
-import {hydrate} from "react-dom";
 import {HelmetProvider} from "react-helmet-async";
 
 const {props, context} = getServerData();

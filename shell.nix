e460753ccf0863b3c091b0fdf432fbd3661cf3e5
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-07-25 19:49:45 -0400

    Missing

--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ mkShell {
     # Needed for our script below to work.
     SOURCE_DATE_EPOCH=$(date +%s);
     export DATABASE_NAME="reactivated"
+    npm install
     setup_script="$(npm bin)/setup_environment.sh"
 
     if [ -f $setup_script ]; then

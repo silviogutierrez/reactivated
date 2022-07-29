bb72fe01ef1efd84aac07fc6d87ae9d099b3b084
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-07-28 22:58:35 -0400

    Fix PostgreSQL driver issue with Nix requirements

--- shell.nix
+++ shell.nix
@@ -3,11 +3,9 @@ let requirements = import ./requirements.nix;
 in with requirements.pkgs;
 
 mkShell {
-  buildInputs = [
-    requirements.production_dependencies
-    requirements.development_dependencies
-    requirements.contributing_dependencies
-  ];
+  buildInputs = requirements.production_dependencies
+    ++ requirements.development_dependencies
+    ++ requirements.contributing_dependencies;
   shellHook = ''
     source "./packages/reactivated/scripts/setup_environment.sh"
   '';

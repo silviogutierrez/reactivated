67d3b8efa625c3da7e20171a4e5fd2c69ad1a43f
Craig Weber <crgwbr@gmail.com>
2022-08-19 16:52:47 -0400

    Improve support for websites with CSP headers
    If the django-csp is installed and in-use, this changes automatically includes
    the CSP nonce in the inline script that reactivated begins each page with. This
    allows reactivate-based sites to use CSP headers without having to enable
    `script-src: 'unsafe-inline'`

--- reactivated/serialization/context_processors.py
+++ reactivated/serialization/context_processors.py
@@ -1,4 +1,12 @@
-from typing import Any, List, Literal, NamedTuple, Type, get_type_hints
+from typing import (
+    Any,
+    List,
+    Literal,
+    NamedTuple,
+    Optional,
+    Type,
+    get_type_hints,
+)
 
 from django.http import HttpRequest
 from django.utils.module_loading import import_string
@@ -10,12 +18,24 @@ from .registry import JSON, Thing
 class Request(NamedTuple):
     path: str
     url: str
+    csp_nonce: Optional[str]
 
     @classmethod
     def get_serialized_value(
         Type: Type["Request"], value: HttpRequest, schema: Thing
     ) -> JSON:
-        return {"path": value.path, "url": value.build_absolute_uri()}
+        serialized = {
+            "path": value.path,
+            "url": value.build_absolute_uri(),
+        }
+        # If django-csp is in use, include the CSP nonce for the request
+        # See https://django-csp.readthedocs.io/en/latest/nonce.html#using-the-generated-csp-nonce
+        serialized["csp_nonce"] = (
+            str(value.csp_nonce)
+            if hasattr(value, "csp_nonce")
+            else None
+        )
+        return serialized
 
 
 class RequestProcessor(NamedTuple):

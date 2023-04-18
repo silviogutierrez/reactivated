d90094849bfc038cd81256be1cc62054f1b6a5ef
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2023-04-18 13:32:33 -0400

    Tweak

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ in with requirements.pkgs;
 mkShell {
   buildInputs = requirements.production_dependencies
     ++ requirements.development_dependencies
-    ++ requirements.contributing_dependencies;
+    ++ requirements.contributing_dependencies ++ [ requirements.pkgs.nix ];
   shellHook = ''
     source "./packages/reactivated/scripts/setup_environment.sh"
   '';

b87cd90cff3827fc82c89f755146fc5b98d95af9
Silvio <silviogutierrez@gmail.com>
2023-06-13 17:51:18 -0400

    Update deploy.sh

--- development/scripts/deploy.sh
+++ development/scripts/deploy.sh
@@ -6,4 +6,4 @@ set -e
 (flyctl auth whoami &>/dev/null) || (echo "You must first login with 'flyctl auth login' and try again" && exit 1)
 
 flyctl deploy --remote-only
-flyctl ssh console --command "sh migrate.sh"
+flyctl ssh console --command "sh /migrate.sh"

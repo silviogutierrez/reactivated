23e6ee3a1e4a0a9970b5e3aedc305ca49d44711d
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2024-01-17 23:12:42 -0500

    Fixes

--- packages/reactivated/src/conf.tsx
+++ packages/reactivated/src/conf.tsx
@@ -7,17 +7,3 @@ export type Options = {
     };
     render?: (content: JSX.Element) => Promise<JSX.Element>;
 };
-
-export const configure = (options: Options) => options;
-
-export default (customConfigurationImport: {default?: Options} | null) => {
-    const customConfiguration = customConfigurationImport?.default ?? {};
-
-    return {
-        build: {
-            client: customConfiguration.build?.client ?? ((options) => {}),
-            renderer: customConfiguration.build?.renderer ?? ((options) => {}),
-        },
-        render: customConfiguration.render ?? ((content) => Promise.resolve(content)),
-    } satisfies Options;
-};

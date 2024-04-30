86ea46e7df75362e2ac7358fb29c8f62de21223d
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2024-04-30 14:38:49 -0400

    Try this

--- packages/create-django-app/scripts/sync-development.sh
+++ packages/create-django-app/scripts/sync-development.sh
@@ -12,6 +12,6 @@ cp ../../requirements.nix template
 sed -i s#../requirements#./requirements# template/shell.nix
 sed -i s#../node_modules#./node_modules# template/.eslintrc.json
 rm template/server/settings/__init__.py
-ln -fs localhost.py template/settings/__init__.py
+ln -s localhost.py template/server/settings/__init__.py
 rm template/package.json
 cp template/.gitignore template/gitignore.template

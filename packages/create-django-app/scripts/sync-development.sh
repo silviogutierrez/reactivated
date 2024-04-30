dedde16fe026b75c728c99212961be69a6306272
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2024-04-30 14:20:27 -0400

    Again

--- packages/create-django-app/scripts/sync-development.sh
+++ packages/create-django-app/scripts/sync-development.sh
@@ -8,10 +8,10 @@ cd "$SCRIPT_PATH/../"
 rm -rf template
 mkdir template
 rsync -a --filter=':- .gitignore' ../../development/ template/
-ln -s localhost.py template/settings/__init__.py
 cp ../../requirements.nix template
 sed -i s#../requirements#./requirements# template/shell.nix
 sed -i s#../node_modules#./node_modules# template/.eslintrc.json
 rm template/server/settings/__init__.py
+ln -s localhost.py template/settings/__init__.py
 rm template/package.json
 cp template/.gitignore template/gitignore.template

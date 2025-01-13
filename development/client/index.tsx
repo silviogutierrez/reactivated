fce2654fd971cc58281840b0bc2ec2b81ca7619f
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2025-01-13 13:25:13 -0500

    More

--- development/client/index.tsx
+++ development/client/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 
 import {Provider, getServerData, getTemplate} from "@reactivated";
 
@@ -7,9 +8,11 @@ import {createRoot} from "react-dom/client";
 const {props, context} = getServerData();
 const Template = await getTemplate(context);
 
-const root = createRoot(document.getElementById("root")!);
-root.render(
-    <Provider value={context}>
-        <Template {...props} />
-    </Provider>,
+ReactDOM.hydrateRoot(
+    document,
+    <React.StrictMode>
+        <Provider value={context}>
+            <Template {...props} />
+        </Provider>
+    </React.StrictMode>,
 );
--- sample/client/index.tsx
+++ sample/client/index.tsx
@@ -1,45 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import {App} from "reactivated/dist/shell";
-
 import {Provider, getTemplate, getServerData} from "@reactivated";
 
 const {props, context} = getServerData();
 const Template = await getTemplate({template_name: "HelloWorld"});
-console.log(context);
 
 ReactDOM.hydrateRoot(
     document,
     <React.StrictMode>
         <Provider value={context}>
-            <Template />
+            <Template {...props} />
         </Provider>
     </React.StrictMode>,
 );
-
-/*
-import {Provider, getTemplate, getServerData} from "@reactivated";
-
-const {props, context} = getServerData();
-const Template = await getTemplate(context);
-console.log("OK");
-
-ReactDOM.hydrateRoot(
-    document,
-    <React.StrictMode>
-        <PageShell
-            vite=""
-            mode="production"
-            preloadContext={context}
-            preloadProps={props}
-            entryPoint="index"
-        >
-            <Provider value={context}>
-                <Template {...props} />
-            </Provider>
-        </PageShell>
-    </React.StrictMode>,
-    );
-
-*/

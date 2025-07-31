41fd8ea9cd31a3f536551932d856b7800383a932
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2025-07-31 13:22:33 -0400

    ok

--- website/shell.nix
+++ website/shell.nix
@@ -11,17 +11,13 @@ mkShell {
     setup_script="$(npm root)/.bin/setup_environment.sh"
 
     if [ ! -f $setup_script ]; then
-       DID_INSTALL=1
        npm install
     fi
 
     source $setup_script
-
-    if [ "$DID_INSTALL" = "1" ]; then
-        # I don't know why this suddenly stopped working, including SETUPTOOLS_ENABLE_FEATURES
-        # https://github.com/pypa/setuptools/issues/3499
-        # Broken here maybe: https://github.com/pypa/pip/issues/12094
-        pip install -e .. --config-settings editable_mode=compat
-    fi
+    # I don't know why this suddenly stopped working, including SETUPTOOLS_ENABLE_FEATURES
+    # https://github.com/pypa/setuptools/issues/3499
+    # Broken here maybe: https://github.com/pypa/pip/issues/12094
+    pip install -e .. --config-settings editable_mode=compat
   '';
 }

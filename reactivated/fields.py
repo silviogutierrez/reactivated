bcd3c980360e93d98920430c7f3b9396b997751a
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2025-08-28 00:15:12 -0400

    Fix EnumField constraint conflcits

--- reactivated/fields.py
+++ reactivated/fields.py
@@ -181,12 +181,14 @@ class _EnumField(models.CharField[_ST, _GT]):  # , Generic[_ST, _GT]):
         # interpolates the values.
         #
         # Fortunately, we can create a name dynamically.
-        cls._meta.constraints.append(
+        # Insert the enum constraint at the beginning to ensure it's applied before other constraints
+        cls._meta.constraints.insert(
+            0,
             EnumConstraint(
                 members=self.enum._member_names_,
                 field_name=name,
                 name=self.get_enum_name(),
-            )
+            ),
         )
 
     def get_enum_name(self) -> str:
